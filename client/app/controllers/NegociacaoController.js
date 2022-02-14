9c75ad1dcb6093272bf900f3223af4e433817fe9
raffa <raffaeleloi121@gmail.com>
2022-02-14 15:46:38 -0300

    finalização capítulo 7

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,6 @@
 class NegociacaoController {
   constructor() {
-    let $ = document.querySelector.bind(document); // Precisa do bind porque o this da função tem que referenciar o document
+    const $ = document.querySelector.bind(document); // Precisa do bind porque o this da função tem que referenciar o document
 
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
@@ -10,13 +10,20 @@ class NegociacaoController {
 
     this._negociacoesView = new NegociacoesView('#negociacoes');
     this._negociacoesView.update(this._negociacoes);
+
+    this._mensagem = new Mensagem();
+
+    this._mensagemView = new MensagemView('#mensagemView');
+    this._mensagemView.update(this._mensagem);
   }
 
   adiciona(event) {
     event.preventDefault();
 
     this._negociacoes.adiciona(this._criaNegociacao());
+    this._mensagem.texto = "Negociação adicionada com sucesso"; 
     this._negociacoesView.update(this._negociacoes);
+    this._mensagemView.update(this._mensagem);
 
     this._limpaFormulario();
   }
